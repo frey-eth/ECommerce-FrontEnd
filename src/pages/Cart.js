a99a2a9b0c987f7105f34066df15df6dbb803c23
Phạm Văn Dương <duongpham321dz@gmail.com>
2023-10-15 19:18:44 +0700

    cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BreadCrumb } from "../components/BreadCrumb";
 import watch from "../images/watch.jpg";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { Link } from "react-router-dom";
 const Cart = () => {
   return (
     <>
@@ -33,7 +35,7 @@ const Cart = () => {
                 <div className="cart-col-2 d-flex align-items-center">
                   <h5 className="price">$100.00</h5>
                 </div>
-                <div className="cart-col-3 d-flex align-items-center">
+                <div className="cart-col-3 d-flex align-items-center gap-15">
                   <div className="">
                     <input
                       type="number"
@@ -45,8 +47,27 @@ const Cart = () => {
                       style={{ width: "70px" }}
                     />
                   </div>
+                  <RiDeleteBin6Line size="24" className="text-danger" />
                 </div>
-                <div className="cart-col-4"></div>
+                <div className="cart-col-4 d-flex align-items-center">
+                  <h5 className="price">$100.00</h5>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 py-2 mt-4">
+              <div className="d-flex justify-content-between">
+                <div className="">
+                  <Link className="button" to="/product">
+                    Continue To Shopping
+                  </Link>
+                </div>
+                <div className="d-flex align-items-end flex-column">
+                  <h4>Total: $1000</h4>
+                  <p>Taxes and shipping calculate at checkout</p>
+                  <Link className="button" to="/checkout">
+                    Checkout
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
