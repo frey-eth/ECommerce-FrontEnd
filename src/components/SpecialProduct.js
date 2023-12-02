4d15f87d782cfbc68884a0838ec3eedafa50f1e5
Phạm Văn Dương <duongpham321dz@gmail.com>
2023-12-02 22:59:22 +0700

    update

--- src/components/SpecialProduct.js
+++ src/components/SpecialProduct.js
@@ -6,10 +6,14 @@ function SpecialProduct(props) {
   const { title, brand, images, totalRatings, price, sold, quantity } = props;
   return (
     <div className="col-6">
-      <div className="special-product-card">
+      <div className="special-product-card h-100">
         <div className="d-flex">
           <div>
-            <img src={images[0].url} alt="watch" className="img-fluid" />
+            <img
+              src={images[0].url}
+              alt="watch"
+              className="img-fluid h-auto d-flex"
+            />
           </div>
           <div className="special-product-content">
             <h5 className="brand">{brand}</h5>
@@ -22,12 +26,12 @@ function SpecialProduct(props) {
               activeColor="#ffd700"
             />
             <p className="price">
-              <span className="red-p">${price}</span>
-              <strike className="m-xl-2">$700</strike>
+              <span className="red-p">${price * 0.75}</span>
+              <strike className="m-xl-2">${price}</strike>
             </p>
             <div className="discount-till d-flex align-items-center gap-10">
-              <p className="mb-0">
-                <b>5 </b>days
+              <p className="mb-0 d-flex">
+                <b>5</b>days
               </p>
               <div className="d-flex gap-15 align-items-center">
                 <span className="badge p-2 bg-warning">1</span>:
